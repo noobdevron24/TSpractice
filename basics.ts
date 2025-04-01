928017e145acc1b06510b3df0a9a68b350c9de34
Ron Manguerra <ronforrestron24@gmail.com>
2025-04-01 15:12:41 +0800

    practice TS

--- basics.ts
+++ basics.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+let userName: string;
+let userAge: number;
+
+userAge = 33;
+userName = 'Ron'; 
+
+const user =(name: string, age: number) => {
+  console.log(`Hello my name is ${name}, and i am ${age} years old`);
+}
+
+user(userName, userAge);
