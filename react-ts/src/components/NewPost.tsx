47c0929971a1a0072f487e5aeba0df909b023d10
Ron Manguerra <ronforrestron24@gmail.com>
2025-04-02 14:50:20 +0800

    update

--- react-ts/src/components/NewPost.tsx
+++ react-ts/src/components/NewPost.tsx
@@ -1,12 +1,22 @@
 import classes from './NewPost.module.css'
-
+import { ChangeEvent } from 'react'
 function NewPost() {
+  
+  function changeBodyHandler(event: ChangeEvent<HTMLTextAreaElement>) {
+    const body = event.target.value;
+    console.log(body)
+  }
+
   return (
     <form className={classes.form}>
+     <p> 
       <input type="text" />
       <label htmlFor="author">Your name</label>
-      <textarea id='body' required rows={3} />
+    </p>
+    <p>
+      <textarea id='body' required rows={3} onChange={changeBodyHandler} />
       <label htmlFor="text">Text</label>
+    </p>
       <button>Add Post</button>
     </form>
   )
